96c2680d273144c1357d2563ea6bf48bf8284bb5
ConnieMarie <constance.marie.strahan@gmail.com>
2022-10-10 16:06:34 -0500

    tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,16 +26,13 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne({
+  Product.findByPk(req.params.id, {
     include: [
       {
         model: Category,
         model: Tag,
       },
     ],
-    where: {
-      id: req.params.id,
-    }
   })
     .then((prodData) => {
       if (!prodData) {
